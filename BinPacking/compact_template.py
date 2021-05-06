bb1cb35a583166182706a59e10d401ba4b87e6d8
Marco Chiarandini <marco@imada.sdu.dk>
2021-05-06 09:05:46 +0200

    added templates for bin packing

--- BinPacking/compact_template.py
+++ BinPacking/compact_template.py
@@ -6,24 +6,9 @@ import itertools
 def bpp(s, B):
     n = len(s)
     U = len(FFD(s, B))
-    model = po.ConcreteModel("bpp")
-    model.var_indices = po.Set(initialize=itertools.product(range(n), range(U)))
-    model.x = model.Var(model.var_indices, within=po.Binary)
-    model.y = model.Var(range(U), within=po.Binary)
+    
+    # Your Gurobi model for the compact formulation
 
-    model.z = po.Objective(po.quicksum(model.y[j] for j in range(U)), sense=po.minimize)
-
-    model.assign = model.ConstraintList()
-    for i in range(n):
-        model.assign.add(expr=po.quicksum(model.x[i, j] for j in range(U)) == 1)
-    for j in range(U):
-        model.add(po.quicksum(s[i]*model.x[i, j]
-                              for i in range(n)) <= B*model.y[j], "Capac(%s)" % j)
-    # for j in range(U):
-    #    for i in range(n):
-    #        model.addCons(model.x[i, j] <= model.y[j], "Strong(%s,%s)" % (i, j))
-
-    # model.data = x, y
     return model
 
 
@@ -31,12 +16,9 @@ def solveBinPacking(s, B):
     n = len(s)
     U = len(FFD(s, B))
     model = bpp(s, B)
-    solver = po.SolverFactory('glpk')
-    results = solver.solve(model)
+    model.optimize()
     bins = [[] for i in range(U)]
-    for (i, j) in model.var_indices:
-        if model.x[i, j]() > .5:
-            bins[j].append(s[i])
+    # Get the contents of the bins from your model 
     for i in range(bins.count([])):
         bins.remove([])
     for b in bins:
