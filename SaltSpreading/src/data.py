9ab99a7a45a8f008615790df68af3486d9ddf5a4
Marco Chiarandini <marco@imada.sdu.dk>
2021-06-12 16:21:12 +0200

    updated data.py to handle inconsistency in the data file kerteminde

--- SaltSpreading/src/data.py
+++ SaltSpreading/src/data.py
@@ -18,20 +18,45 @@ class Data:
         self.E_R = {} # required edges (listed only in version _from > _to) 
         self.A_R = {} # required arcs
               
-
+        
+    def u_turn_set(self):
+        U = set()
+        for node in self.nodes:
+            a_u = set()
+            a_plus_u = set()
+            a_minus_u = set()
+            for edge in self.E_R:
+                if edge[0]==node: 
+                    a_u.add(edge[1])
+                elif edge[1]==node:
+                    a_u.add(edge[0])
+            for arc in list(self.A.keys()) + list(self.A_R.keys()):
+                if arc[0]==node:
+                    a_plus_u.add(arc[1])
+                if arc[1]==node:
+                    a_minus_u.add(arc[0])
+            if len(a_u)==1 and len(a_minus_u)==0 and len(a_plus_u)==0:
+                U.add(node)
+            if len(a_u)==0 and len(a_minus_u)==1 and a_minus_u==a_plus_u:
+                U.add(node)
+            #print(a_u,a_minus_u,a_plus_u)
+        return U
 
 
     def is_req_edge(self, edge):
         return (edge[0],edge[1]) in self.E_R or (edge[1],edge[0]) in self.E_R
+    
+    def is_req_arc(self, edge):
+        return (edge[0],edge[1]) in self.A_R or (edge[1],edge[0]) in self.A_R
 
-        
+
     def read_from_file(self,filename):
         self.outfile = os.path.split(filename)[-1].split(".")[0]
         reader = csv.reader(open(filename, "r"),delimiter=",")
         sys.stdout.write("Read " + filename + "\n")
 
         m3_of_salt_per_m2 = 30*10**(-6) # m^3/m^2
-        capacity_in_m3 = 2 * 12.3 # in m^3
+        capacity_in_m3 = 12.3 # in m^3
         capacity = capacity_in_m3 / (m3_of_salt_per_m2 * 1.0) # capacity in m^2
 
         self.vehicles = {1: {"home": 2,"capacity": capacity},
@@ -53,36 +78,32 @@ class Data:
                     width = float(elem[k+3].strip())
                     status = int(elem[k+1].strip())
                     demand = length * width
-                    
-                    if (_from,_to) not in self.A:
-                        self.A.update({(_from, _to): {"len": length}})
-
-                    if (_to,_from) not in self.A:
-                        self.A.update({(_to, _from): {"len": length}})
 
                     if status == 1: # saltes i valgfri retning
-                        #self.R.append((_from, _to))
-                        if not self.is_req_edge( (_from,_to) ):
+                        #self.R.append((_fro, _to))
+                        if not self.is_req_edge( (_from,_to) ) and not self.is_req_arc((_from,_to)):
                             if _from < _to:
-                                self.E_R.update( {(_from, _to): {"dem":demand}} )
+                                self.E_R.update( {(_from, _to): {"dem":demand, "len": length}} )
                             else:
-                                self.E_R.update( {(_to, _from): {"dem":demand}} )
+                                self.E_R.update( {(_to, _from): {"dem":demand, "len": length}} )
                     elif status == 2:# saltes i denne retning
-                        #self.R.append((_from, _to))
+                        #self.R.append((_fro, _to))
                         if (_from,_to) not in self.A_R:
-                            self.A_R.update( {(_from, _to): {"dem":demand}} )
+                            self.A_R.update( {(_from, _to): {"dem":demand, "len": length}} )
                     ## status 3 is redundant: if an arc is named but not declared with status 1 or 2 then
                     ## no need _to visit it
-                    elif status == 3:# can be left out
-                        continue
-                        ## POSSIBLE ERROR: can we visit this in both directions???
-                    #    self.A.append((_from, _to))
+                    elif status == 3:# can be left out ## POSSIBLE ERROR: can we visit this in both directions???
+                        if (_from,_to) not in self.A:
+                            self.A.update({(_from, _to): {"dem":demand, "len": length}})
+                        if (_to,_from) not in self.A:
+                            self.A.update({(_to, _from): {"dem":demand, "len": length}})
+
                     else:
                         sys.exit("Status of arc not recognised")
-            except ValueError as ve:
-                pass 
-                #raise ValueError(elem) from ve
-
+            except ValueError:
+                pass
+        
+        self.U = self.u_turn_set()
 
 
 
@@ -96,24 +117,25 @@ class Data:
         self.depots = {}
 
         multidict = { 
-                (1,2): [3,1],
-                (1,3): [4,1],
-                (1,4): [4,1],
-                (1,5): [3,1],
+                (1,2): [1,3],
+                (1,3): [1,4],
+                (1,4): [1,4],
+                (1,5): [1,3],
                 (2,3): [1,1],
                 (3,4): [1,1],
-                (3,6): [5,1],
+                (3,6): [1,5],
                 (4,5): [1,1],
-                (4,7): [0,1],
-                (6,7): [14,1]
+                (4,7): [1,0],
+                (6,7): [1,14]
                 }
 
         #E = [(i,j) for (i,j) in multidict]
-        self.A = {(i,j): {"len": multidict[(i,j)][0]} for (i,j) in multidict}
-        self.A.update({(j,i): {"len": multidict[(i,j)][0]} for (i,j) in multidict})
-        
-        self.E_R = {(i,j): {"dem": multidict[(i,j)][1]} for (i,j) in multidict if multidict[(i,j)][1]>0}
+        self.A = {(i,j): {"len": multidict[(i,j)][0]} for (i,j) in multidict if multidict[(i,j)][1]==0}
+        self.A.update({(j,i): {"len": multidict[(i,j)][0]} for (i,j) in multidict if multidict[(i,j)][1]==0})
+                 
+        self.E_R = {(i,j): {"len": multidict[(i,j)][0], "dem": multidict[(i,j)][1]} for (i,j) in multidict if multidict[(i,j)][1]>0}
 
+        self.U = self.u_turn_set()
 
 
 
@@ -127,29 +149,28 @@ class Data:
         self.depots = {}
 
         multidict_edges = {
-            (1,2): [4,1],
-            (1,5): [3,1],
-            (2,3): [3,1],
+            (1,2): [1,4],
+            (1,5): [1,3],
+            (2,3): [1,3],
             (3,4): [1,1],
-            (3,5): [2,1]
+            (3,5): [1,2]
             }
 
         multidict_arcs = {
-            (1,3): [5,1],
-            (4,5): [0,1]
+            (1,3): [1,5],
+            (4,5): [1,0]
             }
 
         #E = [(i,j) for (i,j) in multidict]
-        self.A = {(i,j): {"len": multidict[(i,j)][0]} for (i,j) in multidict_edges}
-        self.A.update({(j,i): {"len": multidict[(i,j)][0]} for (i,j) in multidict_edges})
-        self.A.update({(i,j): {"len": multidict[(i,j)][0]} for (i,j) in multidict_arcs})
+        self.A = {(i,j): {"len": multidict_edges[(i,j)][0]} for (i,j) in multidict_edges}
+        self.A.update({(j,i): {"len": multidict_edges[(i,j)][0]} for (i,j) in multidict_edges})
+        self.A.update({(i,j): {"len": multidict_arcs[(i,j)][0]} for (i,j) in multidict_arcs})
         
         self.E_R = {(i,j): {"dem": multidict_edges[(i,j)][1]} for (i,j) in multidict_edges if multidict_edges[(i,j)][1]>0}
         self.A_R = {(i,j): {"dem": multidict_arcs[(i,j)][1]} for (i,j) in multidict_arcs if multidict_arcs[(i,j)][1]>0}
 
-        
-
-
+        self.U = u_turn_set()
+ 
 
     def load_example_multivisit(self):
         
@@ -161,18 +182,18 @@ class Data:
         self.depots = {}
 
         multidict_edges = { 
-            (1,2): [4,1],
-            (1,3): [3,1],
-            (2,5): [3,1],
+            (1,2): [1,4],
+            (1,3): [1,3],
+            (2,5): [1,3],
             (5,6): [1,1],
-            (6,7): [2,1],
-            (3,4): [4,1]
+            (6,7): [1,2],
+            (3,4): [1,4]
             }
         multidict_arcs = { 
-            (3,2): [5,1],
+            (3,2): [1,5],
             (4,1): [1,1],
-            (6,3): [2,1],
-            (7,4): [3,1]
+            (6,3): [1,2],
+            (7,4): [1,3]
             }
 
         self.A = {(i,j): {"len": multidict[(i,j)][0]} for (i,j) in multidict_edges}
@@ -193,7 +214,8 @@ class Data:
 
         self.depots = {7: {"refill":50.0}}
 
-        E = {(0,1), (1,2), (2,4), (2,3), (1,3), (3,4), (2,5), (5,7), (4,7), (5,7), (5,6)}
+        E = {(0,1), (2,4), (2,3), (1,3), (2,5), (5,7), (4,7), (5,7)}
+        A = {(4,3), (5,6)}
         A_R = {(3,4): 20,(6,5): 30}
         E_R={(1,2): 30}
         def distance(node1,node2):
@@ -201,10 +223,13 @@ class Data:
 
         self.A = {(i,j): {"len": distance(self.nodes[i],self.nodes[j])} for (i,j) in E}
         self.A.update({(j,i): {"len": distance(self.nodes[i],self.nodes[j])} for (i,j) in E})
-        
-        self.A_R = {(i,j): {"dem": A_R[(i,j)]} for (i,j) in A_R}
-        self.E_R = {(i,j): {"dem": E_R[(i,j)]} for (i,j) in E_R}
+        self.A.update({(i,j): {"len": distance(self.nodes[i],self.nodes[j])} for (i,j) in A})
 
+        self.A_R = {(i,j): {"len": distance(self.nodes[i],self.nodes[j]), "dem": A_R[(i,j)]} for (i,j) in A_R}
+        self.E_R = {(i,j): {"len": distance(self.nodes[i],self.nodes[j]), "dem": E_R[(i,j)]} for (i,j) in E_R}
+
+        self.U = self.u_turn_set()
+        #print(self.U)
 
 
 
@@ -238,8 +263,9 @@ class Data:
 
         G.add_edges_from(self.E_R.keys())
         H.add_edges_from(self.A.keys())
+        H.add_edges_from(self.A_R.keys())
 
-        edge_labels = {(i,j):(self.E_R[(i,j)]["dem"],self.A[(i,j)]["len"]) for (i,j) in self.E_R}
+        edge_labels = {(i,j):(self.E_R[(i,j)]["dem"],self.E_R[(i,j)]["len"]) for (i,j) in self.E_R}
 
         pos = {v:self.nodes[v] for v in self.nodes}
         if not all(map(lambda x: True if any(pos[x]) else False, pos)):
@@ -248,7 +274,7 @@ class Data:
         nx.draw_networkx(G, pos=pos, with_labels = True)
         nx.draw_networkx_edge_labels(G,pos=pos,edge_labels = edge_labels,font_size = 12)
 
-        edge_labels = {(i,j): (self.A_R[(i,j)]["dem"],self.A[(i,j)]["len"]) for (i,j) in self.A_R}
+        edge_labels = {(i,j): (self.A_R[(i,j)]["dem"],self.A_R[(i,j)]["len"]) for (i,j) in self.A_R}
         nx.draw_networkx_edges(H, pos=pos,  arrows = True)
         nx.draw_networkx_edge_labels(H,pos=pos,edge_labels = edge_labels,font_size = 12)
 
